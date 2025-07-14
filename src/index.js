71894bbaa8b2643e7ed8e1556aba76d0dd9751c1
miguelflores-0906 <80940342+miguelflores-0906@users.noreply.github.com>
2025-07-14 16:05:14 +0800

    Added Scryfall API calls

--- src/index.js
+++ src/index.js
@@ -1,18 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
-function App() {
-  return (
-    <div>
-      <h1>Hello, World!</h1>
-    </div>
-  );
-}
+import Search from './Search';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Search />
   </React.StrictMode>
 );
 
